b785075027b0e34ebb6286a9d06c97032dc8a4c5
ttexan1 <tetsuji.higashi@geniee.co.jp>
2021-09-01 15:19:28 +0900

    typescript and copy of eslint-plugin-simple-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const rulesDirPlugin = require('eslint-plugin-rulesdir');
 rulesDirPlugin.RULES_DIR = 'eslint/rules';
 
@@ -8,12 +9,29 @@ module.exports = {
   },
   extends: [
     'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:import/errors',
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
+    tsconfigRootDir: '.',
   },
-  plugins: ['rulesdir'],
+  plugins: [
+    'rulesdir',
+    '@typescript-eslint',
+    'import',
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.js'],
+      },
+    },
+  },
   rules: {
     quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'always'],
@@ -22,5 +40,17 @@ module.exports = {
     // 0は無効、1は警告、2はエラー
     'rulesdir/no-literal-call': 2,
     'rulesdir/no-double-byte-char': ['warn', 'always'],
+    'rulesdir/import-sort': 2,
+
+    // 'sort-imports': 0,
+    // 'import/order': [2, {
+
+    //   alphabetize: {
+    //     order: 'asc',
+    //     caseInsensitive: true,
+    //   },
+    //   groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+    //   'newlines-between': 'always', // ignore|always|always-and-inside-groups|never
+    // }],
   },
 };
